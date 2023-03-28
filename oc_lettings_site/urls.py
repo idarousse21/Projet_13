2f76a3f58eb3a2361368c0218a093575e8477774
idarousse <idarousse.i@hotmail.fr>
2023-03-28 18:44:03 +0200

    #

--- oc_lettings_site/urls.py
+++ oc_lettings_site/urls.py
@@ -1,11 +1,16 @@
 from django.contrib import admin
 from django.urls import path, include
-
 from . import views
+
+
+def trigger_error(request):
+    division_by_zero = 1 / 0
+
 
 urlpatterns = [
     path("", views.index, name="index"),
     path("admin/", admin.site.urls),
     path("", include("lettings.urls", namespace="lettings")),
     path("", include("profiles.urls", namespace="profiles")),
+    path("sentry-debug/", trigger_error),
 ]
